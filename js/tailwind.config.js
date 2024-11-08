5242dbe4a085ba44bb0721d2038613e2225f6e1f
Afridul369 <afridul.dev@gmail.com>
2024-11-08 13:05:20 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        
+        colors: {
+          clifford: '#da373d',
+          bgg:'rgba(248, 228, 188, 0.734)',
+          bggg:'rgba(248, 228, 188, 0.934)',
+          aug:'rgba(228, 233, 233, 0.921)',
+        },
+        fontFamily: {
+           sans : ['Open Sans'],
+           play : ['Playfair Display'],
+           pap : ['Paprika'],
+           work : ['Work Sans'],
+           nuni : ['Nunito'],
+          },
+          maxWidth: {
+            container : '1220px',
+            containergg : '1150px',
+          },
+
+      }
+    }
+  }
